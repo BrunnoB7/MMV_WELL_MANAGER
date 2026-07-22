3be21de9929676f1425228241448aaddce42dd6c
BrunnoB7 <98768938+BrunnoB7@users.noreply.github.com>
2026-07-22 11:43:00 -0300

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -285,7 +285,6 @@ def dashboard():
     )
 
     in_progress_activities = (DashboardService.get_in_progress_activities())
-    st.write("DEBUG atividades em andamento:",in_progress_activities,)
     pending = DashboardService.get_critical_pending()
     priorities = DashboardService.get_priorities()
 
